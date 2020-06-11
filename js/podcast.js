4ade320349a1934324580a27c4f78f08b17bf4ba
Roosevelt Padilha <roosevelttgp@gmail.com>
2020-06-11 20:43:34 -0300

    new images

--- js/podcast.js
+++ js/podcast.js
@@ -3,7 +3,7 @@ const realPodcasts = [
     id: '88.5',
     title: 'Syntax',
     description: '[Inglês] Desenvolvimento geral. Apresentado por Wes Bos e Scott Tolinski.',
-    img: '1.png',
+    img: '1.jpg',
     link: 'https://syntax.fm/',
   },
   {
@@ -17,42 +17,42 @@ const realPodcasts = [
     id: '92.3',
     title: 'Lambda3',
     description: 'Desenvolvimento e diversidade. Apresentado pelo pessoal da Lambda3.',
-    img: '3.jpeg',
+    img: '3.jpg',
     link: 'https://www.lambda3.com.br/lambda3-podcast/'
   },
   {
     id: '88.5',
     title: 'DevNaEstrada',
     description: 'Desenvolviemnto em geral e histórias de carreira de desenvolvedores.',
-    img: '4.png',
+    img: '4.jpg',
     link: 'https://devnaestrada.com.br/'
   },
   {
     id: '95.8',
     title: 'Hipsters.tech',
     description: 'Desenvolvimento em geral. Apresentado pelo pessoal da Caelum/Alura.',
-    img: '5.jpeg',
+    img: '5.jpg',
     link: 'https://hipsters.tech/'
   },
   {
     id: '98.3',
     title: 'NerdCast',
     description: 'Cultura Nerd. Apresentado pelo Jovem Nerd e Azhagal.',
-    img: '6.jpeg',
+    img: '6.jpg',
     link: 'https://jovemnerd.com.br/nerdcast/'
   },
   {
     id: '100.4',
     title: 'Área de Transferência',
     description: 'Tecnologias em geral.',
-    img: '7.png',
+    img: '7.jpg',
     link: 'https://areadetransferencia.com.br/'
   },
   {
     id: '105.3',
     title: 'Tecnocast',
     description: 'Tecnologias em geral. Apresentado pelo pessoal do Tecnoblog',
-    img: '8.jpeg',
+    img: '8.jpg',
     link: 'https://soundcloud.com/tecnoblog'
   },
 ]
